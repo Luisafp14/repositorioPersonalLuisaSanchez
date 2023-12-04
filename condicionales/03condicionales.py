2c85a0530825d391aa10d7ab3766801f91433d77
Luisafp14 <140007341+Luisafp14@users.noreply.github.com>
2023-12-04 03:06:32 -0500

    Delete condicionales directory

--- condicionales/03condicionales.py
+++
@@ -1,9 +0,0 @@
-# 3 negativo o positivo
-
-numero=int(input("Digite un numero "))
-
-if numero < 0:
-    input("El numero digitado es negativo")
-else:
-    input("El numero digitado es positivo")
-print("Fin del programa") 
