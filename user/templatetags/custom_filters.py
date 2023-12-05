801fa91d82a2ad5e7ab4d1b57eca41d863a1037a
Luisafp14 <140007341+Luisafp14@users.noreply.github.com>
2023-12-05 14:35:25 -0500

    Delete user directory

--- user/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='price_total')
-def price_total(servicios):
-    return sum([servicio.price for servicio in servicios])
